68c91d48c4d8557111c0110f94fff6d5842628e2
Shohei Hagiwara <hagiwara.shohei@gmail.com>
2020-03-05 21:47:45 +0900

    Add add number feature

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
 class GridCell extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     let { cellValue, addCellValue, row, col } = this.props;
     return <span onClick={() => { addCellValue(row, col) }}>{cellValue}</span>;
@@ -17,16 +11,11 @@ class GridCell extends React.Component {
 }
 
 class GridRow extends React.Component {
-
-  constructor(props) {
-    super(props)
-  }
-
   render() {
     let { rowValues, addCellValue, row } = this.props;
     let rowComps = [];
     for (let [index, cellValue] of rowValues.entries()) {
-      rowComps.push(<GridCell cellValue={cellValue} addCellValue={addCellValue} row={row} col={index}/>)
+      rowComps.push(<GridCell cellValue={cellValue} addCellValue={addCellValue} row={row} col={index} />)
     }
     return <div>{rowComps}</div>;
   }
@@ -50,13 +39,18 @@ class SudokuGrid extends React.Component {
         ]
     };
     this.addCellValue = this.addCellValue.bind(this);
+    this.solve = this.solve.bind(this);
   }
 
   addCellValue(row, col) {
     this.setState(state => {
+
       // deep copy
       let updatedGrid = JSON.parse(JSON.stringify(state.grid));
-      updatedGrid[row][col] = updatedGrid[row][col] + 1;
+
+      // add 1. set it to 0 if 9 now.
+      let currVal = updatedGrid[row][col];
+      updatedGrid[row][col] = currVal === 9 ? 0 : updatedGrid[row][col] + 1;
       return { grid: updatedGrid };
     });
   }
@@ -64,38 +58,66 @@ class SudokuGrid extends React.Component {
   render() {
     let grid = [];
     for (let [index, row] of this.state.grid.entries()) {
-      grid.push(<GridRow rowValues={row} addCellValue={this.addCellValue} row={index}/>)
+      grid.push(<GridRow rowValues={row} addCellValue={this.addCellValue} row={index} />)
     }
     return (
       <div>
         {grid}
-        <button type="button">解く</button>
+        <button type="button" onClick={this.solve}>解く</button>
+        <button type="button">リセット</button>
       </div>
     );
   }
+
+  isValid(grid) {
+
+    return true;
+  }
+
+  solve(e, row=0, col=0, grid=JSON.parse(JSON.stringify(this.state.grid))) {
+    let currVal = grid[row][col];
+
+    // if the cell does not have value yet, go throuhg 1 to 9 and try if each works.
+    let foundCellValue = false;
+    if (currVal === 0) {
+      for (let num of [1, 2, 3, 4, 5, 6, 7, 8, 9]) {
+        grid[row][col] = num;
+        if (this.isValid(grid)) {
+          foundCellValue = true;
+          break;
+        } else {
+          continue;
+        }
+      }
+    }
+
+    // if not found solution for the cell, fail and stop.
+    if (!foundCellValue) {
+      return;
+    }
+
+    // if the cell is the last one, done.
+    if (row === 8 && col === 8) {
+
+      this.setState({ grid: grid, solved: true });
+    } else {
+      let [nextRow, nextCol] = nextRowCol(row, col);
+      
+      this.solve(e, nextRow, nextCol, grid)
+    }
+  }
+}
+
+function nextRowCol(row, col) {
+  if (col === 8) {
+    row++;
+    col = 0;
+  } else {
+    col++;
+  }
+  return [row, col];
 }
 ReactDOM.render(<SudokuGrid />, document.getElementById('root'));
-
-// function tick() {
-//   let now = new Date();
-//   let secondsStyle = { marginLeft: now.getSeconds() * 5 + "px" };
-//   const element = (
-//     <div>
-//       <h1>Hello, world!</h1>
-//       <h2>It is {now.toLocaleTimeString()}.</h2>
-//       <h3 style={secondsStyle}>{now.getSeconds()}</h3>
-//     </div>
-//   );
-//   // highlight-next-line
-//   ReactDOM.render(element, document.getElementById('root'));
-// }
-
-// setInterval(tick, 1000);
-
-
-
-
-
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
